43e69a3c4ef078d9ac6834e5b6131b0f7311a4e8
Vinicius Ferrari Costa <vinicius311@gmail.com>
2013-08-23 01:00:54 -0700

    first draft

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AroundUni::Application.config.secret_key_base = '69555233f1b4e46805dd27e2b837adf7bb10d414980f5fc30904f1ad1de8bbe3eae16ea16b623e56e0863f9d46b8e1c616670260a23cb448a30f184dcd49146f'
