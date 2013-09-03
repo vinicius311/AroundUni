c70b836c1935689f434320e6d9e9cb7879cbdd5a
Vinicius Ferrari Costa <vinicius311@gmail.com>
2013-09-02 21:38:01 -0700

    adding events

---
+++ app/views/events/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @event, :name, :description, :start_time, :end_time, :location_id, :owner, :address, :verification_count, :created_at, :updated_at
---
+++ db/migrate/20130903043415_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.string :description
+      t.datetime :start_time
+      t.date_time :end_time
+      t.integer :location_id
+      t.integer :owner
+      t.string :address
+      t.integer :verification_count
+
+      t.timestamps
+    end
+  end
+end
