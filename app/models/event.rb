0ecfb544393d4e48f602e94ce91069d9a69198e1
Vinicius Ferrari Costa <vinicius311@gmail.com>
2013-09-30 04:50:34 -0700

    models validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ActiveRecord::Base
   
+  attr_accessor :name, :start_time, :end_time, :description, :constraints, :type  
+  
+  validates :name, :start_time, :description, presence: true  
+  
   has_one :user
   has_one :geolocation
   has_many :comments
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
   
+  
+  attr_accessor :name, :gender, :email, :date_of_birth, :date_of_birth, :type  
+  
+  validates :name, :email, :date_of_birth, presence: true
+  validates :email, uniqueness: true
+  
   has_many :events
   has_many :comments
   has_many :verifications
