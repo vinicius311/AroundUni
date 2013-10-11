85ca825c95d616cd79b60c55f202db1082f119ed
Vinicius Ferrari Costa <vinicius311@gmail.com>
2013-10-10 22:14:20 -0700

    users resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ AroundUni::Application.routes.draw do
   resources :geolocations
   resources :regular_events
   resources :special_events
-
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
