29ae44621eb079bbfa77bb00dcbd6056bc80bb48
Alan Khoury <alankhoury@vlan-2615-10-16-67-184.student.wireless.sydney.edu.au>
2013-10-18 16:13:33 +1100

    added mailer but not yet working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,29 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  def create
+
+def create
+  
     @user = User.new(user_params)
-    @user.save
-    
-    redirect_to @user
+
+  
+  
+ 
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome Email after save
+        UserMailer.welcome_email(@user).deliver
+ 
+        format.html { redirect_to(events_path, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+
 
 private
   def user_params
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
   default from: 'noreplyaroununi@gmail.com'
  
   def welcome_email(user)
+    puts "hello my name is terminator!"
     @user = user
     @url  = 'http://arounduni.com/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
