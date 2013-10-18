5f078cf175af7edcb72c7667221fae58bd222f41
Vinicius Ferrari Costa <vinicius311@gmail.com>
2013-10-18 07:56:41 -0700

    log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
   
-  def new
+def new
 end
 
 def create
   user = User.authenticate(params[:email], params[:password])
   if user
-    session[:user_id] = user.id
-    redirect_to root_url, :notice => "Logged in!"
+    if user.verified == true 
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Logged in!"
+    else
+      flash.now.alert = "Please verify your e-mail."
+    end
   else
     flash.now.alert = "Invalid email or password"
     render "new"
