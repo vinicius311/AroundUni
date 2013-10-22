7813a6e63b207d7cf4d58602ab49194b326f84bb
Vinicius Ferrari Costa <vinicius311@gmail.com>
2013-10-22 03:56:26 -0700

    test geocoder

--- app/models/geolocation.rb
+++ app/models/geolocation.rb
@@ -1,5 +1,7 @@
 class Geolocation < ActiveRecord::Base
-   attr_accessible :latitude, :longitude
+  attr_accessible :latitude, :longitude
   belongs_to :event
-  
+  reverse_geocoded_by :latitude, :longitude,
+  :address => :address
+  after_validation :reverse_geocode
 end
